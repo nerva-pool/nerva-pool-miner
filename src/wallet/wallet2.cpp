968554d974fe9fbee46f52a4e9307eb3950856e9
Angry Wasp <angrywasp@live.com>
2018-11-03 22:32:56 +1100

    Build script changes from slate

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -10318,7 +10318,7 @@ std::string wallet2::make_uri(const std::string &address, const std::string &pay
 //----------------------------------------------------------------------------------------------------
 bool wallet2::parse_uri(const std::string &uri, std::string &address, std::string &payment_id, uint64_t &amount, std::string &tx_description, std::string &recipient_name, std::vector<std::string> &unknown_parameters, std::string &error)
 {
-  if (uri.substr(0, 7) != "nerva:")
+  if (uri.substr(0, 5) != "nerva:")
   {
     error = std::string("URI has wrong scheme (expected \"nerva:\"): ") + uri;
     return false;
