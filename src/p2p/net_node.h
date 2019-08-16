60edb9bee132642a72dcba53548f11386d3fe73e
Angry Wasp <angrywasp@live.com>
2019-08-16 13:01:50 +1000

    Minor fixes

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -253,7 +253,7 @@ namespace nodetool
     virtual bool block_host(const epee::net_utils::network_address &adress, time_t seconds = 0);
     virtual bool unblock_host(const epee::net_utils::network_address &address);
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
-    virtual bool add_peer(const std::string &adress);
+    virtual bool add_peer(const std::string &address);
   private:
     const std::vector<std::string> m_seed_nodes_list =
     {
